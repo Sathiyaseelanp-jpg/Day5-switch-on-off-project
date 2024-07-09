82cfd8e6e2fab355996da85cb7703612da362239
Sathiyaseelanp-jpg <sathiyaseelanP0203@gmail.com>
2024-07-09 14:58:32 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,14 @@
+var bulb=0
+function findbulb()
+{
+    if(bulb==0)
+    {
+        document.getElementById("image").src="img3.PNG"
+        bulb=1;
+    }
+
+    else{
+        document.getElementById("image").src="img.4PNG.jpg"
+        bulb=0;
+    }
+}
